3553c5f974edabf63151eb1f90a268226b358a8f
Robiul92 <robiullawal312@gmail.com>
2023-02-15 07:42:41 +0600

    pdf commponent added

--- src/Component/Booking/BookTickets/BookTickets.js
+++ src/Component/Booking/BookTickets/BookTickets.js
@@ -9,11 +9,43 @@ import {
   Image,
 } from "@react-pdf/renderer";
 
+import sitNo from '../SeateDake/SeateDake';
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "#FFFFFF",
+    flexDirection: 'row'
+    // backgroundColor: '#E4E4E4'
+    
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+console.log();
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Slect</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+
+
 const BookTickets = () => {
     return (
         <div >
-            <PDFViewer className='bg-white' width="90%" height="100%">
-                <h1>Hello</h1>
+            <PDFViewer className='bg-white' width="90%" height="100%" border="none">
+              <p>{sitNo}</p>
+                <MyDocument/>
             </PDFViewer>
         </div>
     );
